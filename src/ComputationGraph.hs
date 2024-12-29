dce2f2dbe50e6ddfa89cfe5b15fc263bea55481e
mzguntalan <mzguntalan@gmail.com>
2024-12-29 12:42:00 +0900

    complete functions for class ComputationNode

--- src/ComputationGraph.hs
+++ src/ComputationGraph.hs
@@ -1,8 +1,8 @@
 module ComputationGraph where
 
 class ComputationNode a where
-    simplifyToValue :: (Num b) => a -> b
-    hash :: a -> Int
+    simplifyToValue :: a -> Float
+    hash :: a -> String
     structEq :: a -> a -> Bool
     hashEq :: a -> a -> Bool
     isPoint :: a -> Bool
@@ -18,3 +18,29 @@ simplifyNodeToPointValue (Add (Point x) (Point y)) = x + y
 simplifyNodeToPointValue (Add r s) = simplifyNodeToPointValue (Add (Point (simplifyNodeToPointValue r)) (Point (simplifyNodeToPointValue s)))
 simplifyNodeToPointValue (Negate (Point x)) = -x
 simplifyNodeToPointValue (Negate r) = simplifyNodeToPointValue (Negate (Point (simplifyNodeToPointValue r)))
+
+hashNode :: Node -> String
+hashNode (Point p) = "point"
+hashNode (Add a b) = "add" ++ hashNode a ++ hashNode b
+hashNode (Negate a) = "negate" ++ hashNode a
+
+hashEqNode :: Node -> Node -> Bool
+hashEqNode a b = hashNode a == hashNode b
+
+isNodePoint :: Node -> Bool
+isNodePoint (Point p) = True
+isNodePoint _ = False
+
+structEqNode :: Node -> Node -> Bool
+structEqNode (Point r) (Point s) = True
+structEqNode (Add a b) (Add c d) = structEqNode a c && structEqNode b d
+
+instance ComputationNode Node where
+    simplifyToValue = simplifyNodeToPointValue
+    hash = hashNode
+    structEq = structEqNode
+    hashEq = hashEqNode
+    isPoint = isNodePoint
+
+possiblySameThread :: (ComputationNode a) => a -> a -> Bool
+possiblySameThread r s = hashEq r s && structEq r s
