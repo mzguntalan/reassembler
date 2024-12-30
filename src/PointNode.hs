83671907a8aabf5dfcb8395dfd5759c409d348ff
mzguntalan <mzguntalan@gmail.com>
2024-12-30 19:54:45 +0900

    Map1 and Map2 is here

--- src/PointNode.hs
+++ src/PointNode.hs
@@ -3,6 +3,8 @@
 
 module PointNode where
 
+import Debug.Trace (trace)
+
 class Node a where
     simplify :: a -> a
     hash :: a -> String
@@ -15,18 +17,28 @@ class Operator a where
 
 data BinaryOperator = Add | Subtract deriving (Enum, Show, Eq)
 
+opMustCollection :: String
+opMustCollection = "Operation must receive a Collection"
+
+opWrongNumParams :: String
+opWrongNumParams = "Operation has wrong Number of Paramters"
+
 instance Operator BinaryOperator where
+    perform _ (Point _) = error opMustCollection
     perform Add (Collection [Point a, Point b]) = Point (a + b)
     perform Add (Collection [a, b]) = perform Add (Collection [simplify a, simplify b])
-    perform Add pointnode = perform Add (simplify pointnode)
+    perform Add (Collection _) = error opWrongNumParams
+    perform Add pointnode = trace ("Add pointnode" ++ show pointnode) (perform Add (simplify pointnode))
     perform Subtract (Collection [Point a, Point b]) = Point (a - b)
     perform Subtract (Collection [a, b]) = perform Subtract (Collection [simplify a, simplify b])
+    perform Subtract (Collection _) = error opWrongNumParams
     perform Subtract pointnode = perform Subtract (simplify pointnode)
     hashOperator = show
 
 data UnaryOperator = Negate deriving (Enum, Show, Eq)
 
 instance Operator UnaryOperator where
+    perform _ (Point _) = error opMustCollection
     perform Negate (Collection [Point a]) = Point (-a)
     perform Negate pointnode = perform Negate (simplify pointnode)
     hashOperator = show
@@ -34,10 +46,11 @@ instance Operator UnaryOperator where
 data CollectOperator = Collect | Zip deriving (Enum, Show, Eq) -- produces a Collection PointNode when simplified
 
 instance Operator CollectOperator where
+    perform _ (Point _) = error opMustCollection
     perform Collect cs = cs
     perform Zip (Collection [Point _, _]) = error "Zip expects [Collection as, Collection bs] got [Point _, _]"
     perform Zip (Collection [_, Point _]) = error "Zip expects [Collection as, Collection bs] got [_, Point _]"
-    perform Zip (Collection [Collection as, Collection bs]) = Collection (map (\(x, y) -> Collection [x, y]) (zip as bs))
+    perform Zip (Collection [Collection as, Collection bs]) = simplify (Collection (map (\(x, y) -> Collection [x, y]) (zip as bs)))
     perform Zip (Collection [pointnodeA, pointnodeB]) = perform Zip (Collection [simplify pointnodeA, simplify pointnodeB])
     perform Zip pointnode = perform Zip (simplify pointnode)
     hashOperator = show
@@ -45,18 +58,28 @@ instance Operator CollectOperator where
 applyOperator :: (Operator a) => a -> PointNode -> PointNode
 applyOperator = Operation
 
-data MapOperator = MapOp deriving (Enum, Show, Eq)
+data MapOperator = Map1 | Map2 deriving (Enum, Show, Eq)
 
 instance Operator MapOperator where
     hashOperator = show
-    perform MapOp (Collection [Operation _ _, Point _]) = error "perform expects [Operation _ _, Collection params] instead found [Operation op _, Point _] "
-    perform MapOp (Collection [Operation op _, Collection params]) =
+    perform _ (Point _) = error opMustCollection
+    perform Map1 (Collection [Operation _ _, Point _]) = error "perform expects [Operation _ _, Collection params] instead found [Operation op _, Point _] "
+    perform Map1 (Collection [Operation op _, Collection params]) =
         simplify (Collection nodes)
       where
         nodes = map (applyOperator op) spreadParams
         spreadParams = map (\x -> Collection [x]) params
-    perform MapOp (Collection [Operation op dummy, pointnode]) = perform MapOp (Collection [Operation op dummy, simplify pointnode])
-    perform MapOp pointnode = perform MapOp (simplify pointnode)
+    perform Map1 (Collection [Operation op dummy, pointnode]) = perform Map1 (Collection [Operation op dummy, simplify pointnode])
+    perform Map1 (Collection _) = error opWrongNumParams
+    perform Map1 pointnode = perform Map1 (simplify pointnode)
+    perform Map2 (Collection [Operation op _, Collection params1, Collection params2]) =
+        if length params1 == length params2
+            then
+                simplify (Collection (zipWith (\x y -> applyOperator op (Collection [x, y])) params1 params2))
+            else error opWrongNumParams
+    perform Map2 (Collection [Operation op dummy, pointnode1, pointnode2]) = perform Map2 (Collection [Operation op dummy, simplify pointnode1, simplify pointnode2])
+    perform Map2 (Collection _) = error opWrongNumParams
+    perform Map2 pointnode = perform Map2 (simplify pointnode)
 
 data PointNode where
     Operation :: (Operator a) => a -> PointNode -> PointNode
@@ -67,7 +90,10 @@ data PointNode where
 simplifyPointNode :: PointNode -> PointNode
 simplifyPointNode (Point a) = Point a
 simplifyPointNode (Collection as) = Collection (map simplifyPointNode as)
-simplifyPointNode (Operation op params) = perform op params
+simplifyPointNode (Operation op (Collection params)) = perform op (Collection params)
+simplifyPointNode (Operation op (Operation op2 params)) = perform op (simplifyPointNode (Operation op2 params))
+simplifyPointNode (Operation _ (Point _)) = error "Operation can only act on a Collection"
+simplifyPointNode (Operation _ Dummy) = Dummy
 simplifyPointNode Dummy = Dummy
 
 instance Node PointNode where
