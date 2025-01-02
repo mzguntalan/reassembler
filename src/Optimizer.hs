8ca803624a824ca2cb0e9aa808ebf82aa3274bb7
mzguntalan <mzguntalan@gmail.com>
2025-01-02 22:14:19 +0900

    Remove now uneeded data

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -1,8 +1,1 @@
 module Optimizer where
-
-class ComputationNode a where
-    simplify :: a -> a
-    hash :: a -> String
-    structEq :: a -> a -> Bool
-    hashEq :: a -> a -> Bool
-    isSimplified :: a -> Bool
