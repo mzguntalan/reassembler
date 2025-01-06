cac297504278131be5eccca6bd5e4c31cab3b80f
mzguntalan <mzguntalan@gmail.com>
2025-01-07 00:09:13 +0900

    output is now seggregated by priority

--- playground/output.hs
+++ playground/output.hs
@@ -1,21 +1,19 @@
 module OutputCompute where
 import PointNode
-add = Operation Add (Collection [Dummy,Dummy])
-sub = Operation Subtract (Collection [Dummy,Dummy])
-negateT = Operation Negate (Collection [Dummy])
-
-m1 = Point 5.5
-m2 = Point (-6.5)
-m3 = Point 1.5
-ms = Collection [m1,m2,m3]
-
-n1 = Point 4.5
-n2 = Point 5.4
 n3 = Point 1.2
-ns = Collection [n1,n2,n3]
+n2 = Point 5.4
+n1 = Point 4.5
+m3 = Point 1.5
+m2 = Point (-6.5)
+m1 = Point 5.5
 
-negateNs = Operation Map1 (Collection [negateT,ns])
+ns = Collection [n1,n2,n3]
+ms = Collection [m1,m2,m3]
+negateT = Operation Negate (Collection [Dummy])
+sub = Operation Subtract (Collection [Dummy,Dummy])
+add = Operation Add (Collection [Dummy,Dummy])
+
 negateMs = Operation Map1 (Collection [negateT,ms])
+negateNs = Operation Map1 (Collection [negateT,ns])
 
 out = Operation Map2 (Collection [add,negateMs,negateNs])
-
