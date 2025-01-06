5e13e21417cd42ceb461d70eac4790be24f69a81
mzguntalan <mzguntalan@gmail.com>
2025-01-06 22:28:48 +0900

    can re-arrange lines according to priority

--- src/ComputeLang.hs
+++ src/ComputeLang.hs
@@ -89,12 +89,17 @@ type LookUp = Map.Map String Int
 thelookuptable = Map.fromList [("Dummy", (-1 :: Int))]
 
 determinePriority :: ExpandedExpression -> LookUp -> Priority
+determinePriority (ExpandedExpression _ _ []) _ = -1
 determinePriority (ExpandedExpression _ funcname parameternames) lookuptable
   | funcname == "Point" = 0
   | otherwise = (foldr min 0 $ map (\x -> Map.findWithDefault 0 x lookuptable) parameternames) + 1
 
-expandedExpressionToAnnotatedExpression :: ExpandedExpression -> LookUp -> AnnotatedExpression
-expandedExpressionToAnnotatedExpression expr lookuptable = AnnotatedExpression expr (determinePriority expr lookuptable)
+expandedExpressionToAnnotatedExpression :: ExpandedExpression -> LookUp -> (AnnotatedExpression, LookUp)
+expandedExpressionToAnnotatedExpression (ExpandedExpression varname funcname params) lookuptable = (AnnotatedExpression expr priority, newlookuptable)
+  where
+    expr = ExpandedExpression varname funcname params
+    priority = determinePriority expr lookuptable
+    newlookuptable = Map.insert varname priority lookuptable
 
 annotatedExpressionToHaskellCode :: AnnotatedExpression -> HaskellCode
 annotatedExpressionToHaskellCode (AnnotatedExpression expandedexpr _) = expressionToHaskellCode . expandedExpressionToExpression $ expandedexpr
@@ -104,12 +109,37 @@ expandedExpressionToExpression (ExpandedExpression varname funcanme paramnames) 
   where
     valbody = intercalate " " (funcanme : paramnames)
 
+sortAnnotatedExpressions :: [AnnotatedExpression] -> [AnnotatedExpression]
+sortAnnotatedExpressions = sortBy sortE
+  where
+    sortE (AnnotatedExpression _ p1) (AnnotatedExpression _ p2) = compare p1 p2
+
+filterAnnotatedExpressions :: [AnnotatedExpression] -> [AnnotatedExpression]
+filterAnnotatedExpressions = filter (\(AnnotatedExpression _ p) -> p >= 0)
+
+emptyAnnotatedExpression :: AnnotatedExpression
+emptyAnnotatedExpression = a where (a, _) = (expandedExpressionToAnnotatedExpression . expressionToExpandedExpression . stringToExpression $ "") thelookuptable
+
+insertEmptyLinesAfterChangeInPriority :: [AnnotatedExpression] -> [AnnotatedExpression]
+insertEmptyLinesAfterChangeInPriority [] = []
+insertEmptyLinesAfterChangeInPriority [a] = [a]
+insertEmptyLinesAfterChangeInPriority ((AnnotatedExpression e1 p1) : (AnnotatedExpression e2 p2 : as))
+  | p1 /= p2 = [AnnotatedExpression e1 p1, emptyAnnotatedExpression] ++ insertEmptyLinesAfterChangeInPriority (AnnotatedExpression e2 p2 : as)
+  | otherwise = [AnnotatedExpression e1 p1] ++ insertEmptyLinesAfterChangeInPriority (AnnotatedExpression e2 p2 : as)
+
 computeToHaskell :: IO ()
 computeToHaskell = do
   content <- Text.readFile "../playground/sample.compute"
-  let processedContent =
+  let expandedExpressions = map (expressionToExpandedExpression . stringToExpression . Text.unpack) (Text.lines content)
+      (annotatedLines, _) = foldl f ([], thelookuptable) expandedExpressions
+        where
+          f (expressions, table) expr = (annotatedExpr : expressions, newtable)
+            where
+              (annotatedExpr, newtable) = expandedExpressionToAnnotatedExpression expr table
+      processedLines = insertEmptyLinesAfterChangeInPriority . filterAnnotatedExpressions . sortAnnotatedExpressions $ annotatedLines
+      processedContent =
         Text.unlines . map Text.pack $
           beginStatement
-            : map (haskellCodeToString . annotatedExpressionToHaskellCode . (`expandedExpressionToAnnotatedExpression` thelookuptable) . expressionToExpandedExpression . stringToExpression . Text.unpack) (Text.lines content)
+            : map (haskellCodeToString . annotatedExpressionToHaskellCode) processedLines
   Text.writeFile "../playground/output.hs" processedContent
   putStrLn "Success"
